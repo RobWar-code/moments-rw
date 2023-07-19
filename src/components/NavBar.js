9703517142fc4b2d810e901df4607a0d2ce7d5cf
RobWar-code <rowaniar@aol.com>
2023-07-19 18:41:18 +0000

    Add page routing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,30 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import { Navbar, Nav} from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 import logo from '../assets/logo.png'
 import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top"><Container>
-    <Navbar.Brand href="#home">
-        <img src={logo} alt="logo" height="45" />
-    </Navbar.Brand>
+    <NavLink to="/">
+        <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
+    </NavLink>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto text-left">
-        <Nav.Link>
+        <NavLink exact to="/" className={styles.NavLink} activeClassName={styles.Active}>
             <i className="fas fa-home"></i>Home
-        </Nav.Link>
-        <Nav.Link>
+        </NavLink>
+        <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
             <i className="fas fa-sign-in-alt"></i>Sign-in
-        </Nav.Link>
-        <Nav.Link>
+        </NavLink>
+        <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
             <i className="fas fa-user-plus"></i>Sign-up
-        </Nav.Link>
+        </NavLink>
         </Nav>
     </Navbar.Collapse>
     </Container>
