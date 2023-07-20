86a142ee52046786a23ba9a0a129fa88f41ff913
RobWar-code <rowaniar@aol.com>
2023-07-20 08:21:31 +0000

    Add sign-in form page and axios url links

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://dj-rest-profiles-7506ba4404dc.herokuapp.com/';
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.withCredentials = true;
