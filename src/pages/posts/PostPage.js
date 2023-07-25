9c0adcaa5e5e41947671c02082bcaf0b90e06ba5
RobWar-code <rowaniar@aol.com>
2023-07-25 18:33:33 +0000

    Add post component to post page

---
+++ src/pages/posts/PostPage.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
+
+function PostPage() {
+  // Extract the id from the url
+  const { id } = useParams();
+  const [post, setPost] = useState({
+    results: [],
+  });
+
+  // Fetch the post when the page is mounted
+  useEffect(() => {
+    const handleMount = async () => {
+        try {
+            // This allows for mutiple gets - although we are only fetching one
+            // on this occasion.
+            const [{ data: post }] = await Promise.all([
+                axiosReq.get(`/posts/${id}`)
+            ]);
+            setPost({results: [post]});
+            console.log(post);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    };
+
+    handleMount();
+  }, [id])
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles for mobile</p>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container className={appStyles.Content}>
+          Comments
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        Popular profiles for desktop
+      </Col>
+    </Row>
+  );
+}
+
+export default PostPage;
