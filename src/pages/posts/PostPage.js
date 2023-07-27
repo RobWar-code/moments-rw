32e1186762f121b6c735ced93d5b92ceb15d874f
RobWar-code <rowaniar@aol.com>
2023-07-27 11:19:09 +0000

    PostsPage has error with results member of posts

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -26,7 +26,6 @@ function PostPage() {
                 axiosReq.get(`/posts/${id}`)
             ]);
             setPost({results: [post]});
-            console.log(post);
         }
         catch (err) {
             console.log(err);
--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
@@ -13,21 +13,44 @@ import { axiosReq } from "../../api/axiosDefaults";
 function PostsPage({message, filter=""}) {
   const [posts, setPosts] = useState({results: []});
   const [hasLoaded, setHasLoaded] = useState(false);
+  // This enables us to track the url page
+  // Bearing in mind that we may have got here from /feed or /liked
   const {pathname} = useLocation();
   
-  const useEffect = async () => {
-    try {
-      const { data } = await axiosReq.get(`/posts/?${filter}`)
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const [{ data: posts }] = await axiosReq.get(`/posts/?${filter}`);
+        setPosts({results: [posts]});
+        console.log(posts.results)
+        setHasLoaded(true);
+      }
+      catch (err) {
+        console.log(err);
+      }
     }
-    catch (err) {
 
-    }
-  }
+    setHasLoaded(false);
+    fetchPosts();
+    // Note that we execute this when either of filter or pathname changes
+  }, [filter, pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
+        { hasLoaded ? (
+            <>
+            { posts.results.length ? (
+              console.log("Got posts for display")
+            ) : (
+              console.log("No posts found")
+            )
+            }
+            </>
+        ) : (
+          console.log('Show loading spinner')
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
